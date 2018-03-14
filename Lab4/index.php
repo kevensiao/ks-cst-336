bce4acceca712e865d7a36f64a8c372733c2c58f
Keven Siao <ksiao@csumb.edu>
2018-03-14 16:09:59 +0000

    db

--- Lab4/index.php
+++ Lab4/index.php
@@ -60,7 +60,22 @@
         <div id="list">
             <?php
         
-            $dbConn = new PDO("mysql:host=localhost;dbname=lab4", 'kevens', 'lol');
+            //$connUrl = getenv('JAWSDB_MARIA_URL');
+            $connUrl = "mysql://trn5zsb0tbly80jw:vac4hxlio0wzkrmb@z1ntn1zv0f1qbh8u.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/mlm03i8f43cxocez";
+            $hasConnUrl = !empty($connUrl);
+            
+            $connParts = null;
+            if ($hasConnUrl) {
+                $connParts = parse_url($connUrl);
+            }
+            
+            //var_dump($hasConnUrl);
+            $host = $hasConnUrl ? $connParts['host'] : getenv('IP');
+            $dbname = $hasConnUrl ? ltrim($connParts['path'],'/') : 'crime_tips';
+            $username = $hasConnUrl ? $connParts['user'] : getenv('C9_USER');
+            $password = $hasConnUrl ? $connParts['pass'] : '';
+            
+            $dbConn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
             
             if ($_GET['type']!='')
             {
@@ -83,10 +98,10 @@
                 echo 'It is ordered by '. $_GET['name'];
                 if($_GET['price']!=''){
                     echo ' and price.';
-                    $input = $dbConn->query('SELECT * FROM device WHERE deviceName OR deviceType LIKE \'%'. $_GET['name'] .'%\' ORDER BY price');
+                    $input = $dbConn->query('SELECT * FROM device WHERE deviceName LIKE \'%'. $_GET['name'] .'%\' OR deviceType LIKE \'%'. $_GET['name'] .'%\' ORDER BY price');
                 } 
                 else {
-                    $input = $dbConn->query('SELECT * FROM device WHERE deviceName OR deviceType LIKE \'%'. $_GET['name'] .'%\' ORDER BY deviceName');
+                    $input = $dbConn->query('SELECT * FROM device WHERE deviceName LIKE \'%'. $_GET['name'] .'%\' OR deviceType LIKE \'%'. $_GET['name'] .'%\' ORDER BY deviceName');
                 }
             } 
             
